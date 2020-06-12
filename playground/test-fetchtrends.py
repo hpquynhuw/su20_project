a3278b45d5a83113a7451aa28cb47761d5ed8291
Quynh <hp.quynh@yahoo.com>
2020-06-11 20:36:40 -0700

    test fetching trends

---
+++ playground/test-fetchtrends.py
@@ -0,0 +1,25 @@
+import json
+import requests
+from secrets import twitter_token
+
+
+class Test_FetchTrends:
+
+    def __init__(self):
+        trend_url="https://api.twitter.com/1.1/trends/place.json?id="
+        headers={"Authorization": "Bearer {}".format(twitter_token)}
+        params = {
+            'id': ['2490383','4118', '1105779', '44418', '2295019'],
+            'names': ['seattle','toronto', 'sydney','london','newdelhi']
+            }
+        for i, j in zip(params['id'],params['names']):
+            url=trend_url+i
+            res=requests.get(url, headers=headers)
+            tweet_data=res.json()
+
+            out_file = open(f"{j}.json", "w")
+            json.dump(tweet_data, out_file, indent=6)
+            out_file.close()
+
+if __name__ == '__main__':
+    cp=Test_FetchTrends()
