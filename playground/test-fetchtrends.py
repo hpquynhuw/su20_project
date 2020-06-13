90b5be76d1a00039d568573f3977149e4e84cc92
Quynh <hp.quynh@yahoo.com>
2020-06-12 19:42:43 -0700

    update trends with dates, tested GET-search

--- playground/test-fetchtrends.py
+++ playground/test-fetchtrends.py
@@ -1,23 +1,24 @@
 import json
 import requests
 from secrets import twitter_token
-
+from secrets import base_url
+from datetime import date
 
 class Test_FetchTrends:
-
     def __init__(self):
-        trend_url="https://api.twitter.com/1.1/trends/place.json?id="
+        trend_url=base_url+"trends/place.json?id="
         headers={"Authorization": "Bearer {}".format(twitter_token)}
         params = {
-            'id': ['2490383','4118', '1105779', '44418', '2295019'],
-            'names': ['seattle','toronto', 'sydney','london','newdelhi']
+            'id': ['2490383','4118', '1105779', '44418'],
+            'names': ['seattle','toronto', 'sydney','london']
             }
+        today = date.today()
         for i, j in zip(params['id'],params['names']):
             url=trend_url+i
             res=requests.get(url, headers=headers)
             tweet_data=res.json()
 
-            out_file = open(f"{j}.json", "w")
+            out_file = open(f"{j}_{today}.json", "w")
             json.dump(tweet_data, out_file, indent=6)
             out_file.close()
 
