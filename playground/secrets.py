28205d10aeab1c8f4246dc6dbb1dede44c0b75ec
Quynh <hp.quynh@yahoo.com>
2020-07-12 14:39:44 -0700

    hide secrets

--- playground/secrets.py
+++ playground/secrets.py
@@ -1,2 +1,1 @@
-twitter_token="AAAAAAAAAAAAAAAAAAAAAO5PFAEAAAAAnoUc%2Fd2Il%2BZ6KQc7HShNsy7rxLU%3D60JcWQIOtliYFabGPYfS6yWJsgSd7tjyDBQEgheQO1hkVccTjh"
-base_url="https://api.twitter.com/1.1/"
+twitter_token="
