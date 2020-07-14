9cddcf4d5ac0144e07d725158c5627cfff4954d0
Quynh <hp.quynh@yahoo.com>
2020-07-13 17:28:49 -0700

    wow quinh

--- playground/flask/app.py
+++ playground/flask/app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
+import json
+import os
+from datetime import date
+
+now = date.today()  # end date
+
 
 app = Flask(__name__)
 
@@ -6,6 +12,12 @@ app = Flask(__name__)
 def hello_world():
     return render_template('test.html')
 
-@app.route('/data/')
-def data():
-    return 'hello world'
+@app.route('/home', methods=['GET'])
+def back_home():
+    return redirect(url_for('hello_world'));
+
+@app.route('/<place>', methods=['GET'])
+def get_trends_today(place):
+    filename = os.path.join(app.static_folder, 'data', f'{place}_{now}.json')
+    data = json.load(open(filename))
+    return render_template('list.html', data=data)
